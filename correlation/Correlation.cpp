3405c396998732406c22c332a10f970925d8179c
nathanforest <nathanlee@nevada.unr.edu>
2020-11-18 17:31:26 -0800

    image correlation
    finally applied correlation over the init image with the pattern mask

--- correlation/Correlation.cpp
+++ correlation/Correlation.cpp
@@ -8,16 +8,18 @@ using namespace std;
 
 #include "image.h"
 
-int readImageHeader(char[], int&, int&, int&, bool&);
-int readImage(char[], ImageType&);
-int writeImage(char[], ImageType&);
+int readImageHeader(char[], int &, int &, int &, bool &);
+int readImage(char[], ImageType &);
+int writeImage(char[], ImageType &);
 
 int rows, cols, Q, val, maskVal;
 
-void normalize(ImageType& initImage, vector<int> correlatedVector, ImageType& outputImage) {
+void normalize(ImageType &initImage, vector<int> correlatedVector, ImageType &outputImage)
+{
   int max = 0;
   int min = 1000000000;
   int value;
+  cout << "sit";
 
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
@@ -39,56 +41,42 @@ void normalize(ImageType& initImage, vector<int> correlatedVector, ImageType& ou
   }
 }
 
-
-
-void correlate(ImageType& initImage, ImageType& maskImage, int maskHeight, int maskWidth, ImageType& outputImage) {
+void correlate(ImageType &initImage, ImageType &maskImage, int maskHeight, int maskWidth, ImageType &outputImage)
+{
   vector<int> correlationVector;
   maskHeight = maskHeight / 2;
-  // cout << maskHeight << endl;
   maskWidth = maskWidth / 2;
-  // cout << maskWidth << endl;
 
-  for (int i = 0; i < rows; i++) {
-    // cout << "shit";
-    for (int j = 0; j < 1; j++) {
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < cols; j++)
     {
-      // cout << "shit";
-      int sum = 0;
-
-      // maybe the hiegh and width need to be truncated. 
-      // even one tep out of bounds in an array would casuse a seg fault
-      for (int k = -maskHeight; k < maskHeight; k++)
       {
-        // cout << k << endl;
-        // cout << "shit" << endl;
-        // maskHeight = floor(maskHeight / 2);
-        // sum += 1;
-        // cout << maskHeight << endl;
-        // cout << sum << endl;
-        for (int l = -maskWidth / 2; l < maskWidth / 2; l++)
+        int sum = 0;
+        for (int k = -maskHeight; k < maskHeight; k++)
         {
-          //this if condition is what is the problem.
-          if(i + k < 0 || i + k <= maskWidth || j + l < 0 || j + l <= maskHeight) {
-            sum += 0;
-            // cout << "shit";
-            // cout << sum << endl;
-          }
-          else {
-            initImage.getPixelVal((i + k), (j + l), val);
-            // cout << val;
-            maskImage.getPixelVal((k + maskHeight / 2), (l + maskWidth / 2), maskVal);
-            sum += val * maskVal;
-            
+          for (int l = -maskWidth; l < maskWidth; l++)
+          {
+            if (i + k < 0 || i + k >= rows || j + l < 0 || j + l >= cols)
+            {
+              sum += 0;
+            }
+            else
+            {
+              initImage.getPixelVal((i + k), (j + l), val);
+              // cout << val;
+              maskImage.getPixelVal((k + maskHeight), (l + maskWidth), maskVal);
+              sum += val * maskVal;
+              // cout << sum << endl;
+            }
           }
         }
+        correlationVector.push_back(sum);
       }
-      correlationVector.push_back(sum);
     }
   }
   normalize(initImage, correlationVector, outputImage);
-}}
-
-
+}
 
 int main(int argc, char *argv[])
 {
@@ -112,7 +100,6 @@ int main(int argc, char *argv[])
   correlate(initImage, maskImage, maskRows, maskCols, outputImage);
 
   writeImage(argv[3], outputImage);
-
 
   return (1);
 }
